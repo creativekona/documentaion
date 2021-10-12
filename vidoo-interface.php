b2054080591c7e730e49547ade58295c2444bc01
Rakesh Kumar <creativekona@gmail.com>
2021-10-12 19:49:23 +0530

    Last one

--- vidoo-interface.php
+++ vidoo-interface.php
@@ -110,7 +110,7 @@
                                     
                                     <h4> Clips </h4>
                                     <p>This feature contains the recorded clip(s) for each camera. </p>
-                                    <p>When you click favourite button, this history/clips will not delete...</p>
+                                    <p>When you click favourite button, this history/clips will not delete.</p>
                                     <img src="assets/images/inf-15.jpg" class="img-fluid pb-4" />
 
                                     <p>To select a camera that you would like to display, click the camera selection drop-down button, then click Search after selecting.</p>
@@ -129,7 +129,7 @@
                                     
                                     <h4> History </h4>
                                     <p>This feature shows the past recorded clip(s) retained in the vidoo App.</p>
-                                    <p>When you click favourite button, this history/clips will not delete...</p>
+                                    <p>When you click favourite button, this history/clips will not delete.</p>
                                     <img src="assets/images/inf-19.jpg" class="img-fluid pb-4" />
 
                                 </div><!--//section-block-->
