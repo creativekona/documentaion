4d4046e4deb4af2e4dc75fa2469e713775ab45c0
Rakesh Kumar <creativekona@gmail.com>
2021-10-13 14:01:53 +0530

    Changes

--- index.php
+++ index.php
@@ -3,11 +3,38 @@
         case '/':
             require 'home.php';
             break;
-        case '/download':
+        case '/wiki':
             require 'download.php';
             break;
+        case '/wiki/download':
+            require 'download.php';
+            break;
+            
+        case '/wiki/contact-us':
+            require 'contact-us.php';
+            break;
+            
+        case '/wiki/installation':
+            require 'installation.php';
+            break;
+            
+        case '/wiki/vidoo-interface':
+            require 'vidoo-interface.php';
+            break;
+            
+        case '/wiki/vidoo-login':
+            require 'vidoo-login.php';
+            break;
+            
+        case '/wiki/virtual-box-installation':
+            require 'virtual-box-installation.php';
+            break;
+
         default:
-            http_response_code(404);
-            exit('Not Found');
+            //http_response_code(404);
+            //exit('Not Found');
+            
+            require 'home.php';
+            break;
     }
 ?>
